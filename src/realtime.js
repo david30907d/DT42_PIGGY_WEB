ca589b8d03327ed732ee1de9d85867c846a94f58
davidtnfsh <davidtnfsh@gmail.com>
2020-08-22 23:12:08 +0800

    feat: 🎸 show realtime image to web page

--- src/realtime.js
+++ src/realtime.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { Image } from "semantic-ui-react";
 import LablesOfRealtime from "./labelsOfRealtime.js";
-const src =
-  "https://cdn.shopify.com/s/files/1/1952/8341/products/SakuraCreditFrameA_2020@2x.jpg?v=1587461579";
+const src = "http://127.0.0.1:8000/videofeed";
 
 const RealtimeImage = () => (
   <div>
