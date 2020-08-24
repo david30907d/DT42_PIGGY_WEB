7c35e37da0c2efe92d699abf4a77377134bc0526
davidtnfsh <davidtnfsh@gmail.com>
2020-08-24 21:20:39 +0800

    feat: 🎸 send camera id as query string to API

--- src/realtime.js
+++ src/realtime.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Image } from "semantic-ui-react";
 import LablesOfRealtime from "./labelsOfRealtime.js";
-const src = "http://127.0.0.1:8000/videofeed";
 
-const RealtimeImage = () => (
-  <div>
-    <center>
-      <LablesOfRealtime />
-      <Image src={src} size="massive" />
-    </center>
-  </div>
-);
+const RealtimeImage = () => {
+  const src = `http://127.0.0.1:8000/videofeed${window.location.search}`;
+  return (
+    <div>
+      <center>
+        <LablesOfRealtime />
+        <Image src={src} size="massive" />
+      </center>
+    </div>
+  );
+};
 
 export default RealtimeImage;
