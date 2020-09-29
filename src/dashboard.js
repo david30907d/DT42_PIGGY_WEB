b4c9a7efd99d7c6e7a05a5c85ab65c8a940a46e6
davidtnfsh <davidtnfsh@gmail.com>
2020-09-29 09:32:05 +0800

    fix: 🐛 add unique key to each elements

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Header, Table, Icon, Label } from "semantic-ui-react";
-
 export default class TableExamplePadded extends React.Component {
   constructor(props) {
     super(props);
@@ -25,7 +24,9 @@ export default class TableExamplePadded extends React.Component {
         <Table.Header>
           <Table.Row>
             {Array.from(Array(this.dashboard_days).keys()).map((value) => (
-              <Table.HeaderCell singleLine>{value}</Table.HeaderCell>
+              <Table.HeaderCell singleLine key={value}>
+                {value}
+              </Table.HeaderCell>
             ))}
           </Table.Row>
         </Table.Header>
@@ -37,36 +38,36 @@ export default class TableExamplePadded extends React.Component {
             this.step_of_hours_per_day
           ).map((hour) => {
             return (
-              <Table.Row>
-                <Table.Cell>
-                  <Header as="h2" textAlign="center">
+              <Table.Row key={hour}>
+                <Table.Cell key={hour}>
+                  <Header as="h2" textAlign="center" key={hour}>
                     {hour}:00
                   </Header>
                 </Table.Cell>
                 {this.range(1, this.dashboard_days, 1).map((day) => {
                   return (
-                    <Table.Cell>
-                      {hour % 1 === false && (
-                        <Label>
-                          <Icon name="heart" />
+                    <Table.Cell key={`${day}_${hour}`}>
+                      {hour % 1 === 0 && (
+                        <Label key={`${day}_heart`}>
+                          <Icon name="heart" color="red" />
                           {hour}
                         </Label>
                       )}
-                      {hour % 2 === false && (
-                        <Label>
-                          <Icon name="frown" />
+                      {hour % 2 === 0 && (
+                        <Label key={`${day}_frown`}>
+                          <Icon name="frown" color="purple" />
                           {hour}
                         </Label>
                       )}
-                      {hour % 3 === false && (
-                        <Label>
-                          <Icon name="hospital" />
+                      {hour % 3 === 0 && (
+                        <Label key={`${day}_hospital`}>
+                          <Icon name="hospital" color="blue" />
                           {hour}
                         </Label>
                       )}
-                      {hour % 4 === false && (
-                        <Label>
-                          <Icon name="woman" />
+                      {hour % 4 === 0 && (
+                        <Label key={`${day}_woman`}>
+                          <Icon name="woman" color="teal" />
                           {hour}
                         </Label>
                       )}
